e239d037a4b2a6736fcb8c668c7be9c5810474e1
ksesly <ks.you.04@gmail.com>
2023-03-17 18:50:24 +0200

    errror nofile1

--- inc/uls.h
+++ inc/uls.h
@@ -2,9 +2,15 @@
 
 #include "../libmx/inc/libmx.h"
 #include <dirent.h> 
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
 
 //errors
-void mx_uls_errors_checker(int argc, char* argv[]);
+void mx_uls_errors_checker(int argc);
+void mx_uncreated_file(char* oshibka);
 
 //void mx_format_error(void);
 
