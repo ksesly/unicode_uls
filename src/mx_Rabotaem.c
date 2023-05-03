9d50029750654932ffb570ae1387b7e7fb167d80
ksesly <ks.you.04@gmail.com>
2023-05-03 11:00:38 -0700

    fix: -R flag

--- src/mx_Rabotaem.c
+++ src/mx_Rabotaem.c
@@ -9,8 +9,8 @@ void mx_Rls(int argc, char *argv[]){
         struct stat file_statistics;
         t_list *spisok = mx_return_spisok(dir);
 
-        mx_printstr(".:\n");
-        mx_print_column(spisok);
+        //mx_printstr(".:\n");
+        //mx_print_column(spisok);
         mx_print_columnnnnnnnn(spisok);
         for (t_list *i = spisok; i != NULL; i = i->next){
             char *buff = mx_strdup(".");
@@ -20,6 +20,7 @@ void mx_Rls(int argc, char *argv[]){
             if (stat(i->data, &file_statistics) == -1) continue;
             
             if (S_ISDIR(file_statistics.st_mode)){
+                mx_printstr("\n");
                 mx_tipa_recyrsia(buff);
             }
             else {
@@ -35,28 +36,30 @@ void mx_Rls(int argc, char *argv[]){
         t_list *dir_spisok = mx_list_dir(argc, argv, 2);
         if (dir_spisok != NULL && dir_spisok->next == NULL && file_spisok == NULL) {
 
-            // DIR *dir = opendir(dir_spisok->data);
-            // if (!dir) {
-            //     mx_uncreated_file(dir_spisok->data); 
-            // }
-            // t_list *sp = mx_return_spisok(dir);
+             DIR *dir = opendir(dir_spisok->data);
+            if (!dir) {
+                 mx_uncreated_file(dir_spisok->data); 
+             }
+             t_list *sp = mx_return_spisok(dir);
 
-            
-            mx_tipa_recyrsia(dir_spisok->data);
-            //closedir(dir);
+             mx_print_columnnnnnnnn(sp);
+            //mx_tipa_recyrsia(dir_spisok->data);
+            closedir(dir);
             //mx_printchar('\n');
         }
         if (file_spisok != NULL) {
-            for (t_list *i = file_spisok; i != NULL; i = i->next){
-                if (i->next == NULL){
-                    mx_printstr(i->data);
-                    mx_printstr("\n");
-                }
-                else {
-                    mx_printstr(i->data);
-                    mx_printstr("  ");
-                }
-            }
+            //for (t_list *i = file_spisok; i != NULL; i = i->next){
+                // if (i->next == NULL){
+                //     mx_printstr(i->data);
+                //     mx_printstr("\n");
+                // }
+                // else {
+                //     mx_printstr(i->data);
+                //     mx_printstr("  ");
+                // }
+
+                mx_print_columnnnnnnnn(file_spisok);
+            //}
             flag = true;
         }     
 
@@ -88,56 +91,23 @@ void mx_tipa_recyrsia(char *path){
 
 
     t_list *spisok = mx_return_spisok(dir);
-    mx_printstr("\n");
+    //mx_printstr("\n");
     mx_printstr(path);
     mx_printstr(":\n");
     mx_print_columnnnnnnnn(spisok);
     for (t_list *i = spisok; i != NULL; i = i->next){
-
         struct stat file_statistics;
-        // mx_printstr("(");
-        // mx_printstr(i->data);
-        // mx_printstr(")");
-
-        // char *route_in_file = (char *)malloc(mx_strlen(path));
-        // strcpy(route_in_file, path);
-        // strcat(route_in_file, "/");
-        // strcat(route_in_file, i->data);
-
-        // mx_strjoin(route_in_file, "/");
-        // mx_strjoin(route_in_file, i->data);
-
         char route_in_file[1024];
-        // // mx_strcat(route_in_file, path);
-        // mx_strcat(route_in_file, "/");
-        // mx_strcat(route_in_file, i->data);
-
-        // mx_printstr("(");
-        // mx_printstr(route_in_file);
-        // mx_printstr(")");
-        // mx_printstr("\n");
 
         sprintf(route_in_file, "%s/%s", path, i->data);
-
-
         if (stat(route_in_file, &file_statistics) == -1){
-            // free(route_in_file);
             continue;
         }
         if (S_ISDIR(file_statistics.st_mode)){
+            mx_printstr("\n");
             mx_tipa_recyrsia(route_in_file);
-            // if (route_in_file != NULL){
-            //     mx_printstr("456");
-            //     free(route_in_file);
-            //     route_in_file = NULL;
-            // }
         }
-        // if (route_in_file != NULL){
-        //     mx_printstr("123");
-        //     free(route_in_file);
-        //     route_in_file = NULL;
-        // }
     }
-    // free(route_in_file);
+
     closedir(dir);
 }
